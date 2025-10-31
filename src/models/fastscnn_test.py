ace01463d30297fe4b09ee31ffe1be55cd2ff986
mdaefiol <m.daefiol@gmail.com>
2025-10-30 22:06:53 -0300

    Update FCN baseline to process and visualize 50 training images with Fast-SCNN, limit CPU usage

--- src/models/fastscnn_test.py
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/Tramac/Fast-SCNN-pytorch.git
-cd Fast-SCNN-pytorch
-pip install -r requirements.txt
-
-python demo.py --cpu --weight-path ./pretrained/fast_scnn_citys.pth --img-path ./samples/demo.png
